0b84e805283ab1c910ac03ae9f961e3c50a7d114
Morning-Star <150641177+M-MDI@users.noreply.github.com>
2024-09-26 13:42:20 +0100

    Update struct.go

--- utils/struct.go
+++ utils/struct.go
@@ -4,13 +4,18 @@ import (
 	"net"
 	"sync"
 )
-
+	/**
+		<!--				comment 				--!>
+	*/
 type Client struct {
 	conn net.Conn
 	name string
 }
-
+	/**
+		<!--				comment 				--!>
+	*/
 var (
 	clients = make(map[Client]bool)
+	messages  = []string{}
 	mutex   = &sync.Mutex{}
 )
